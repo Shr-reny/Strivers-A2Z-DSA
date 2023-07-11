29505599d94927329dfb5575bbe1de4204edc7a1
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-11 13:10:37 +0530

    Create sum of even and odd digits.cpp

---
+++ Chap 1-(Learn the basics)/chap 1.1-(things to know in C++)/sum of even and odd digits.cpp
@@ -0,0 +1,21 @@
+/*
+The aim is to return sum of the even and odd digits present in a number. By digits we mean the numbers and not index.
+for ex:- n=132456 so sum of even=12 and sum of odd=9 */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;  
+	cin >> n;
+	int odd=0, even=0;
+	while(n){
+		if(n%2==0) even = even+n%10;
+		else odd = odd+n%10;
+		n = n/10;
+	}
+	cout <<even << " " <<odd;
+}
+
+/* Input= 552245
+  Output= 8 15*/
