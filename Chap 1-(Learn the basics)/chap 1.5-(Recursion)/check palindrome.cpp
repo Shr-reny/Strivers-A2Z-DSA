a51465b1c25cd0fe6bef581c068b2a4b03827a05
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-21 23:56:16 +0530

    Update check palindrome.cpp

--- Chap 1-(Learn the basics)/chap 1.5-(Recursion)/check palindrome.cpp
+++ Chap 1-(Learn the basics)/chap 1.5-(Recursion)/check palindrome.cpp
@@ -9,3 +9,24 @@ return pal(i+1,s);
 bool isPalindrome(string& s) {
     return pal(0,s);
 }
+
+/* Leetcode problem having testcase like "A man, a plan, a canal: Panama" */
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int i=0, j=s.length();
+       while(i<=j)
+       {
+           if(!isalnum(s[i])) {i++; continue;}
+           if(!isalnum(s[j])) {j--; continue;}
+           if(tolower(s[i])!=tolower(s[j])) {return false;}
+           else
+           {
+               i++;
+               j--;
+           }
+       }
+       return true;
+    }
+};
