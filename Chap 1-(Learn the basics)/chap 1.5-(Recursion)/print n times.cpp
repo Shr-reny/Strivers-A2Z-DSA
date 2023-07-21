2978b63d2a1a9261ae7f5836971b92fb42f91462
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-21 17:28:51 +0530

    Create print n times.cpp

---
+++ Chap 1-(Learn the basics)/chap 1.5-(Recursion)/print n times.cpp
@@ -0,0 +1,11 @@
+/* Our aim is to use functions to implement recursion without using loops. This function will print n times */
+
+vector<string>ans;
+/* you have to declare vector outside orelse every time value will be pushed in a new vector */
+vector<string> printNTimes(int n) {
+  string val="Coding Ninjas";
+	if(n==0)
+	return ans;
+	ans.push_back(val);
+	return printNTimes(n-1);
+}
