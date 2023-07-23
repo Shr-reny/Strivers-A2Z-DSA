ed36409d5b51fa2eb7a52c7c62ff8e2964f03c54
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-23 12:20:44 +0530

    Create insertion sort.cpp

---
+++ Chap 2-(Sorting)/insertion sort.cpp
@@ -0,0 +1,16 @@
+/* Insertion sort compares an element with it's left element and places it at it's right position. So, array starts to be sorted from 
+beginning and we iterate only in unsorted part */
+/* Time complexity in worst and avg cases are O(n*n) and in best case is O(N) when array is sorted */
+
+void insertion_sort(int arr[], int n)
+{
+  for(int i=0;i<n;i++)
+    {
+      int j=i;
+      while(j>0 && arr[j-1]>arr[j])
+        {
+          swap(arr[j],arr[j-1];
+          j--;
+        }
+    }
+}
