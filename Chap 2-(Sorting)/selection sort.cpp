a2c6cdd43c3b028178fa5feb136354fe95fe3f60
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-23 11:22:20 +0530

    Create selection sort.cpp

---
+++ Chap 2-(Sorting)/selection sort.cpp
@@ -0,0 +1,19 @@
+/* It means we will select the unsorted array then find the minimum element and swap it with the first element in the selected range */
+/* Time complexity = O(n*n) for all best, worst, average cases */
+
+void selectionSort(vector<int>&arr) {
+    int n=arr.size();
+    for(int i=0;i<n;i++)
+    {
+        int min=i;
+        for(int j=i;j<n;j++)
+        {
+            if(arr[min]>arr[j]) 
+            {
+                int temp=arr[min];
+                arr[min]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
