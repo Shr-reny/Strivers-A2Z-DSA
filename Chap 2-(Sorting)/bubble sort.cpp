5d74eab23cfc49f219327b985d192b680256cdbf
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-24 18:35:48 +0530

    Update bubble sort.cpp

--- Chap 2-(Sorting)/bubble sort.cpp
+++ Chap 2-(Sorting)/bubble sort.cpp
@@ -19,3 +19,23 @@ void bubbleSort(int arr[], int n)
         }
     }
 }
+
+/* RECURSIVE APPROACH */
+
+void bubbleSort(int arr[], int n) 
+{
+    if(n==1) return;
+   
+        for(int j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            // swap(arr[j],arr[j+1]);
+            }
+        }
+
+    return bubbleSort(arr,n-1);
+}
