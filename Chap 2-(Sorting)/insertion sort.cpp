d547eac80bba9e1593dfd0a17b2865249152be95
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-24 18:40:01 +0530

    Update insertion sort.cpp

--- Chap 2-(Sorting)/insertion sort.cpp
+++ Chap 2-(Sorting)/insertion sort.cpp
@@ -14,3 +14,21 @@ void insertion_sort(int arr[], int n)
         }
     }
 }
+
+/*RECURSIVE APPROACH */
+
+void insertion_sort(int arr[], int i, int n) {
+
+    // Base Case: i == n.
+    if (i == n) return;
+
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+
+    insertion_sort(arr, i + 1, n);
+}
