6b5eef3c9caf97951726589128ae27b5a4af9220
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-24 22:27:02 +0530

    Create second largest & smallest.cpp

---
+++ Chap 3-(Arrays)/Easy/second largest & smallest.cpp
@@ -0,0 +1,29 @@
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+   int l=INT_MIN,sl=INT_MIN;
+   for(int i=0;i<n;i++)
+   {
+       if(a[i]>l)
+       {
+            sl=l;
+           l=a[i];
+       }
+       else if(a[i]>sl && a[i]!=l)
+       {
+           sl=a[i];
+       }
+   }
+   int s=INT_MAX,ss=INT_MAX;
+   for(int i=0;i<n;i++)
+   {
+       if(a[i]<s)
+       {
+            ss=s;
+           s=a[i];
+       }
+       else if(a[i]<ss &&a[i]!=s)
+       {
+           ss=a[i];
+       }
+   }
+   return {sl,ss};
+}
