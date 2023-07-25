0ef162dafdbb16da4bd512e49adc1014da63c2e3
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-25 21:38:16 +0530

    Update left rotate by one.cpp

--- Chap 3-(Arrays)/Easy/left rotate by one.cpp
+++ Chap 3-(Arrays)/Easy/left rotate by one.cpp
@@ -1,4 +1,9 @@
+/* Aim is to rotate the array by one place to the left. */
 
+
+/* BRUTE FORCE:
+ Here we create another vector for storing the elements from second element onwards and then manually pushing the first element at last.
+Here time complexity is O(N) and space complexity is O(N) as well as new array is created */
 
 vector<int> rotateArray(vector<int>& arr, int n) {
    vector<int>temp;
@@ -10,8 +15,10 @@ vector<int> rotateArray(vector<int>& arr, int n) {
    return temp;
 }
 
+/* OPTIMAL SOLUTION:
+Here we don't create extra array and just do swappings within the same one so,
+   Time complexity is O(N) but space complexity is O(1) */
 
-#include <bits/stdc++.h> 
 vector<int> rotateArray(vector<int>& arr, int n) {
     int temp=arr[0];
     for(int i=1;i<n;i++)
