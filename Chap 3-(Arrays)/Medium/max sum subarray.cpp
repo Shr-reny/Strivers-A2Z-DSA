3ecba2af02cc18904deb5e7e7d84c3d21393a729
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-28 20:24:05 +0530

    Create max sum subarray.cpp

---
+++ Chap 3-(Arrays)/Medium/max sum subarray.cpp
@@ -0,0 +1,46 @@
+/* Aim is to find a subarray with max sum from an array */
+
+/* BRUTE FORCE:
+TC is O(n3) and SC is O(1) */
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            int sum = 0;
+
+            //add all the elements of subarray:
+            for (int k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+
+            maxi = max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+/* BETTER SOL:
+TC is O(N2) and SC is   O(1) */
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; // maximum sum
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = max(maxi, sum); // getting the maximum
+        }
+    }
+
+    return maxi;
+}
