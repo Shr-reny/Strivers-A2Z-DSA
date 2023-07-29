d1e671d2356f166dbcf7b0a95567891daea3dc92
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-29 11:31:09 +0530

    Create leaders in array.cpp

---
+++ Chap 3-(Arrays)/Medium/leaders in array.cpp
@@ -0,0 +1,22 @@
+/* leaders are the one which has all elements lower in value following it on right. In this context, the rightmost element is always the leader */
+
+/* OPTIMAL SOL:
+/* TC is O(N) and SC is O(N) as well */
+vector<int> superiorElements(vector<int>&a) {
+    int n=a.size();
+    int maxi=a[n-1];
+        vector<int>ans;
+        ans.push_back(a[n-1]);
+        for(int i=n-2;i>=0;i--)
+        {
+            
+            if(a[i]>maxi)
+            {
+                 maxi=a[i];
+                ans.push_back(a[i]);
+               
+            }
+        }
+        sort(ans.begin(),ans.end()); 
+        return ans;
+}
