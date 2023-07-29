092a71ac8c74795f3626550c0d822bf1fe48ea11
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-07-29 10:02:20 +0530

    Update rearrange +ve and -ve.cpp

--- Chap 3-(Arrays)/Medium/rearrange +ve and -ve.cpp
+++ Chap 3-(Arrays)/Medium/rearrange +ve and -ve.cpp
@@ -50,3 +50,71 @@ public:
         return ans;
     }
 };
+
+
+/* SECOND VARIANT */
+
+/* The positive and negatives should be placed alternatively and the remaining should be placed at the end */
+
+
+/*O(2*N) { The worst case complexity is O(2*N) which is a combination of O(N) of traversing the array to segregate
+into neg and pos array and O(N) for adding the elements alternatively to the main array} and SC is  O(N/2 + N/2) = O(N) */
+
+
+ vector<int> RearrangebySign(vector<int>A, int n){
+    
+  // Define 2 vectors, one for storing positive 
+  // and other for negative elements of the array.
+  vector<int> pos;
+  vector<int> neg;
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+  
+  // If positives are lesser than the negatives.
+  if(pos.size() < neg.size()){
+      
+    // First, fill array alternatively till the point 
+    // where positives and negatives ar equal in number.
+    for(int i=0;i<pos.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+    }
+    
+    // Fill the remaining negatives at the end of the array.
+    int index = pos.size()*2;
+    for(int i = pos.size();i<neg.size();i++){
+        
+        A[index] = neg[i];
+        index++;
+    }
+  }
+  
+  // If negatives are lesser than the positives.
+  else{
+      
+      // First, fill array alternatively till the point 
+      // where positives and negatives ar equal in number.
+      for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+    
+    // Fill the remaining positives at the end of the array.
+    int index = neg.size()*2;
+    for(int i = neg.size();i<pos.size();i++){
+        
+        A[index] = pos[i];
+        index++;
+    }
+  }
+  return A;
+    
+}
+
