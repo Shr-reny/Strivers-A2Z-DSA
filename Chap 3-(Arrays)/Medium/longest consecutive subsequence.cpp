91880d6c17b3933418df2d48ccff7aa1aad8f7ed
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-08-01 19:58:06 +0530

    Update longest consecutive subsequence.cpp

--- Chap 3-(Arrays)/Medium/longest consecutive subsequence.cpp
+++ Chap 3-(Arrays)/Medium/longest consecutive subsequence.cpp
@@ -55,6 +55,37 @@ int longestSuccessiveElements(vector<int>&a) {
    return longest;
 }
 
+/* OPTIMAL SOL:
+TC is O(3*N), O(N) for putting all the elements into the set data structure. After that for every starting element, we are finding the consecutive elements.
+Though we are using nested loops, the set will be traversed at most twice in the worst case. So, the time complexity is O(2*N) instead of O(N2).
+SC is O(N) */
+
+int longestSuccessiveElements(vector<int>&a) {
+   int n=a.size();
+   int longest=1;
+   int cnt=0;
+   unordered_set<int>st;
+   for(int i=0;i<n;i++)
+   {
+       st.insert(a[i]);
+   }
+   for(auto it:st)
+   {
+       if(st.find(it-1)==st.end())
+       {
+           int x=it;
+           cnt=1;
+           while(st.find(x+1)!=st.end())
+           {
+               cnt+=1;
+               x=x+1;
+           }
+           longest=max(longest,cnt);
+       }
+   }
+   return longest;
+}
+
 
 
 
