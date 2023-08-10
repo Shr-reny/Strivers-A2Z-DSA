6fc2038d696b6d3abb0b9795ac84c30af21b8246
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-08-10 09:12:22 +0530

    Create spiral matrix.cpp

---
+++ Chap 3-(Arrays)/Medium/spiral matrix.cpp
@@ -0,0 +1,36 @@
+/* Aim is to print a matrix in a spiral manner like this:
+  
+
+vector<int> spiralMatrix(vector<vector<int>>&mat) {
+    vector<int>ans;
+    int n=mat.size();
+    int m=mat[0].size();
+    int top=0,right=m-1,bottom=n-1,left=0;
+
+    while(top<=bottom && left<=right)
+    {
+        for(int i=left;i<=right;i++)
+        {
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++)
+        {
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for (int i = right; i >= left; i--) {
+              ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
+        }
+        if(left<=right){
+            for (int i = bottom; i >= top; i--) {
+              ans.push_back(mat[i][left]);
+            }
+            left ++;
+        }
+    }
+    return ans;
+}
