59c46dbcc317c7e20814aae8cc790c841c959593
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2023-08-11 10:59:47 +0530

    Create count subarray sum equals k.cpp

---
+++ Chap 3-(Arrays)/Medium/count subarray sum equals k.cpp
@@ -0,0 +1,69 @@
+/* aim is to find a subarray with sum equal to k and count total such subarrays */
+
+/*BRUTE FORCE:
+O(N3) and O(1) */
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+/*BETTER:
+O(N2) AND o(1) */
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+/*OPTIMAL
+O(n) and O(1) */
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
