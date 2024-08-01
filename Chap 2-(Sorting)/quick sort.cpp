a024067276ef6a8fd1f2ae0a4106e78b7a7bd04f
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-08-01 23:10:48 +0530

    Update quick sort.cpp

--- Chap 2-(Sorting)/quick sort.cpp
+++ Chap 2-(Sorting)/quick sort.cpp
@@ -1,6 +1,6 @@
 /* Quicksort also uses the concept of divide and conquer and uses a pivot element to divide. A pivot is chosen, placed at it's right position and
 then the left and right subarrays are sorted. */
-/* Time complexity is O(logn) but it is little better than merge sort in terms of space complexity
+/* Time complexity is O(logn) but it is little better than merge sort in terms of space complexity,
 where quick has O(1) and merge has O(n) */
 
 int partitionArray(int input[], int start, int end) {
