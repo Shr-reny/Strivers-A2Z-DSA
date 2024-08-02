3bd2b34a39f33f58c9047b3f57d70ebda2a1f2c6
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-08-02 12:11:18 +0530

    Update check palindrome.cpp

--- Chap 1-(Learn the basics)/chap 1.5-(Recursion)/check palindrome.cpp
+++ Chap 1-(Learn the basics)/chap 1.5-(Recursion)/check palindrome.cpp
@@ -10,7 +10,10 @@ bool isPalindrome(string& s) {
     return pal(0,s);
 }
 
-/* Leetcode problem having testcase like "A man, a plan, a canal: Panama" */
+/* Leetcode problem having testcase like "A man, a plan, a canal: Panama" 
+By reading the question you may think that first we need to convert the string to desired form 
+and then check if its a valid palindrome or not. But all of that is just distraction you can check valid palindrome by simply using two pointers
+no need to convert. We keep finding characters from start and end, convert them to lowercase and match for palindrome checking*/
 
 class Solution {
 public:
