593aa676f91f816216d79bb9b60704450ee20b80
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-08-12 19:43:34 +0530

    Update bubble sort.cpp

--- Chap 2-(Sorting)/bubble sort.cpp
+++ Chap 2-(Sorting)/bubble sort.cpp
@@ -1,7 +1,7 @@
 /* Bubble sort suggests that we sort by swapping the adjacent elements in ascending order. 
-This swapping continues till there is no more possible swaps in the array */
+This swapping continues till there is no more possible swaps in the array. Array starts getting start from last or bottom */
 /* In one round of swapping largest element will always settle at the bottom so we don't
-need to run till end everytime*/
+need to run till end everytime. Subsequently second last will be placed at it's place and so on*/
 /* Time complexity for avg and worst case is O(n*n) and best case is O(n) when array is sorted */
 
 void bubbleSort(int arr[], int n) 
