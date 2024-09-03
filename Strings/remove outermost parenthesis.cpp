b4d19b9d30682dff4f80658ff2347c4848934877
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-09-03 17:49:09 +0530

    Create remove outermost parenthesis.cpp

---
+++ Strings/remove outermost parenthesis.cpp
@@ -0,0 +1,34 @@
+/* Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())". */
+
+ string removeOuterParentheses(string s) {
+        string str="";
+        int k=0;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+                if(s[i]=='(')
+                {
+                    if(k>0)
+                    {str+=s[i];}
+                    k++;
+                }
+                else
+                {
+                    k--;
+                    if(k>0){
+                    str+=s[i];}
+                }
+            
+        }
+        return str;
+    }
