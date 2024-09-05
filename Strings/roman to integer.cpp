aa54f404faadd8e6578db32a137e332e8e59b12b
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-09-05 13:19:15 +0530

    Create roman to integer.cpp

---
+++ Strings/roman to integer.cpp
@@ -0,0 +1,38 @@
+/* Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 
+12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+*/
+
+int romanToInt(string s) {
+        int i=0;
+        int n=s.length();
+        int num=0;
+        map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        for(int i=0;i<n;i++)
+        {
+            if(mp[s[i]]<mp[s[i+1]]) num-=mp[s[i]];
+            else num+=mp[s[i]];
+        }
+        return num;
+    }
