cb103b58c5b62a0c70e09f3c6b9af804f5be08e8
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-09-11 17:56:36 +0530

    Create Insert at end.cpp

---
+++ Linked List/Single LL/Insert at end.cpp
@@ -0,0 +1,19 @@
+/* 
+
+Given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the modified Linked List.
+Input: LinkedList: 1->2->3->4->5 , x = 6
+Output: 1->2->3->4->5->6
+
+*/
+
+Node *insertAtEnd(Node *head, int x) {
+        Node* temp=head;
+        Node* neww=new Node(x);
+        if(head==NULL) return neww;
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=neww;
+        return head;
+    }
