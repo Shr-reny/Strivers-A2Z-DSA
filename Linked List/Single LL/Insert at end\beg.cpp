550c0079a8bf931842c0a51c416c5d385eeda55b
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-09-18 00:19:36 +0530

    Update Insert at end\beg.cpp

--- Linked List/Single LL/Insert at end\beg.cpp
+++ Linked List/Single LL/Insert at end\beg.cpp
@@ -1,9 +1,7 @@
 /* 
-
 Given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the modified Linked List.
 Input: LinkedList: 1->2->3->4->5 , x = 6
 Output: 1->2->3->4->5->6
-
 */
 
 Node *insertAtEnd(Node *head, int x) {
