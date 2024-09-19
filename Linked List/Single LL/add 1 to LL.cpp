2225caff8e5df385b1cd6aada163ef6e42aedeee
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-09-19 19:51:05 +0530

    Create add 1 to LL.cpp

---
+++ Linked List/Single LL/add 1 to LL.cpp
@@ -0,0 +1,27 @@
+/*
+You are given a linked list where each element in the list is a node and have an integer data.
+You need to add 1 to the number formed by concatinating all the list node numbers together and return the head of the modified linked list. 
+ex:-
+  9->9->null
+  1->0->0->null
+*/
+
+int addf(Node* temp)
+    {
+        if(temp==NULL) return 1;
+        int carry=addf(temp->next);
+        temp->data+=carry;
+        if(temp->data<10) return 0;
+        temp->data=0;
+        return 1;
+    }
+    Node* addOne(Node* head) {
+        int carry= addf(head);
+        if(carry==1)
+        {
+            Node* newN= new Node(1);
+            newN->next=head;
+            head=newN;
+        }
+        return head;
+    }
