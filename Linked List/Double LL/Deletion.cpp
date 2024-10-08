69d9316dadf737782044fbf3becacb61c59a441a
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-10-08 20:23:28 +0530

    Create Deletion.cpp

---
+++ Linked List/Double LL/Deletion.cpp
@@ -0,0 +1,28 @@
+/*
+Deletion at a position k
+*/
+
+Node* delete_head(Node* head){
+        if(head == nullptr || head->next == nullptr) return nullptr;
+        head = head->next;
+        head->prev = nullptr;
+        return head;
+    }
+    Node* deleteNode(Node* head, int x) {
+        Node* temp=head;
+        if(x == 1) return delete_head(head);
+        for(int i=1;i<x;i++)
+        {
+            temp=temp->next;
+        }
+        if(temp && temp->next)
+        {
+            temp->prev->next= temp->next;
+            temp->next->prev=temp->prev;
+        }
+        else if(temp)
+        {
+            temp->prev->next=NULL;
+        }
+        return head;
+    }
