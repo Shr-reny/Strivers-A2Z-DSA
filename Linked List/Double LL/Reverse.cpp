6d4ed0a4b8f35f4ccf2b67f60b0109f4455448de
Shruti Swarupa Dhar <125943681+Shr-reny@users.noreply.github.com>
2024-10-12 00:11:14 +0530

    Update Reverse.cpp

--- Linked List/Double LL/Reverse.cpp
+++ Linked List/Double LL/Reverse.cpp
@@ -3,3 +3,16 @@ Brute force will be to use a stack for reversal;
 Optimal takes O(n) TC and O(1) SC;
 */
 
+DLLNode* reverseDLL(DLLNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        DLLNode* last=NULL;
+        DLLNode* curr=head;
+        while(curr!=NULL)
+        {
+            last=curr->prev;
+            curr->prev=curr->next;
+            curr->next=last;
+            curr=curr->prev;
+        }
+        return last->prev;
+    }
